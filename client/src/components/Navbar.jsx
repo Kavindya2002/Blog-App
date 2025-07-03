3f3c471a13b011875b100a96d46f4ed8208378d7
Kavindy <kavindyamallawa0@gmail.com>
2025-07-03 06:40:03 +0530

    Add logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,10 @@ import { assets } from '../assets/assets'
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 '>
-        <img src={assets} alt="" />
+        <img src={assets.logo} alt="logo" className='w-32 sm:w-44' />
+        <button>Login 
+          <img src={assets.arrow} className='w-3' alt="arrow" />
+        </button>
     </div>
   )
 }
