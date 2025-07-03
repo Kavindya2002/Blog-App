bc22f3d232731c5261eea5b366478caea13b3ef0
Kavindy <kavindyamallawa0@gmail.com>
2025-07-03 07:13:29 +0530

    Add description title for home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Header from '../components/Header'
 
 const Home = () => {
   return (
-    
-      <Navbar/>
+    <>
+    <Navbar />
+    <Header/>
+    </>
+      
+      
    
   )
 }
