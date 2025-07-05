18cb95c3fbf5e72a3b3b630d0c23f98f43ff4ed4
Kavindy <kavindyamallawa0@gmail.com>
2025-07-05 11:29:50 +0530

    Add animations for filters

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { blogCategories } from '../assets/assets'
 import  { useState } from 'react'
+import { motion, spring } from "motion/react"
 
 
 const BlogList = () => {
@@ -13,7 +14,10 @@ const BlogList = () => {
                 <div key={item} className='relative'>
                     <button onClick={()=> setMenu(item)} className={`cursor-pointer text-gray-500 ${menu === item && 'text-white px-4 pt-0.5'}`}>
                         {item}
-                        {menu === item && (<div className='absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full'></div>)}
+                        {menu === item && (
+                            <motion.div layoutId='underline' 
+                            transition={{type:spring , stiffness: 500 , damping:30}}
+                            className='absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full'></motion.div>)}
                         
                     </button>
                 </div>
