b046723b99d2cbecb6250d1f2b80daee75f39a78
Kavindy <kavindyamallawa0@gmail.com>
2025-07-06 12:55:39 +0530

    Add copyright message in footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 bg-primry/3'>
+        <div>
+
+        </div>
+        <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2025 @ QuickBlog Kavindya - All Right Reserved.</p>
+
+    </div>
+  )
+}
+
+export default Footer
