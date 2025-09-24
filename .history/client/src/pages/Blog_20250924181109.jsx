d359a64edb0df73138f3cbc6d6a0b82d67a66b2a
Kavindy <kavindyamallawa0@gmail.com>
2025-09-24 18:22:52 +0530

    create title and date in blog page

--- .history/client/src/pages/Blog_20250924181109.jsx
+++ .history/client/src/pages/Blog_20250924181109.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
 import { blog_data } from '../assets/assets'
 import Navbar from '../components/Navbar'
-import Moment from 'moment'
+import Moment from 
 
 
 const Blog = () => {
@@ -29,11 +29,8 @@ const Blog = () => {
 
     <Navbar/>
       
-      <div className='text-center mt-20 text-gray-600'>
-        <p className='text-primary py-4 font-medium'>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
-        <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
-        <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
-        <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary'>Michael Brown</p>
+      <div>
+        <p>Published on {data.createdAt}</p>
       </div>
 
       <div></div>
